e3e85675fd3e7210ddafa4ca8af156e689a16dea
Alex <aslvrstn@gmail.com>
2022-09-21 15:16:54 -0400

    Cleaned up phantom module, almost

---
+++ phantom_module.py
@@ -0,0 +1,37 @@
+from typing_extensions import ParamSpec, Protocol
+from typing import TypeVar, Generic, TYPE_CHECKING, cast, Type
+from torch.nn import Module
+
+P = ParamSpec("P")
+R = TypeVar("R")
+
+class HasForward(Protocol[P, R]):
+    def forward(self, *args: P.args, **kwargs: P.kwargs) -> R:
+        ...
+
+class PhantomModule(Generic[P, R], Module):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    if TYPE_CHECKING:
+        def __call__(self, *args: P.args, **kwds: P.kwargs) -> R:
+            return super().__call__(*args, **kwds)
+
+def make_typed(cls: Type[HasForward[P, R]]) -> Type[PhantomModule[P, R]]:
+    return cast(Type[PhantomModule[P, R]], cls)
+
+
+# This is non-critical and just an example
+class FunkyNN(PhantomModule):
+    def __init__(self, arg: int) -> None:
+        super().__init__()
+        self.params = ["moooo"]
+    
+    def forward(self, x: int) -> int:
+        return x
+
+model = make_typed(FunkyNN)(arg=3)
+
+out = model(x=1)  # Happy
+out = model(x="str")  # Sad
+print(out)
