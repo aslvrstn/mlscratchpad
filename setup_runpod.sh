e2812be134931bcd1d72cb67c5288982fb2b22bf
Alex <aslvrstn@gmail.com>
2022-10-25 03:07:28 -0400

    Simple runpod script

---
+++ setup_runpod.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# Beforehand
+# cd /workspace
+# git clone https://github.com/aslvrstn/mlscratchpad.git
+
+apt update
+apt install locales
+apt install virtualenv
+cd /workspace/mlscratchpad
+virtualenv -p python3 venv
+source venv/bin/activate
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
+
+pip install wandb==0.12.1
+pip install matplotlib
+pip install git+https://github.com/neelnanda-io/Easy-Transformer.git
+
+# Install VSCode extensions (jupyter + python)
